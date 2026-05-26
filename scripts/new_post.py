2bbc2dc8463fb54d3b279380d5c1f072054c2c38
Connor de la Cruz <connor.c.delacruz@gmail.com>
2026-05-26 14:31:49 -0500

    code cleanup and documentation

--- scripts/new_post.py
+++ scripts/new_post.py
@@ -17,7 +17,12 @@ DATE_STRING_FMT = '%Y-%m-%d'
 # ================================================================================
 
 def format_post_title(post_title_args):
-    # TODO: DOC
+    '''Convert title args to hyphen-separated lowercase string.
+
+    :param post_title_args: Array of words used in post title.
+
+    :return: Hyphen-separated lowercase string.
+    '''
     # TODO: support formatted title arg to insert via frontmatter?
     # TODO: sanitize?
     post_title = '-'.join(post_title_args)
@@ -26,9 +31,11 @@ def format_post_title(post_title_args):
 
 
 def validate_and_format_date_arg(date_arg):
-    '''Sanitize and format date arg.
+    '''Sanitize and format date arg into string formatted 'YYYY-MM-DD'.
 
-    Returns None if a ValueError occurred.
+    :param date_arg: Value passed to --date argument.
+
+    :return: Formatted date string or None if a ValueError occurred.
     '''
     try:
         parsed_date = datetime.strptime(date_arg, DATE_STRING_FMT)
@@ -40,13 +47,26 @@ def validate_and_format_date_arg(date_arg):
 
 
 def format_filename(formatted_post_title, formatted_date_string):
+    '''Format filename of post .md file.
+
+    Format: '<post-title-text>-<YYYY-MM-DD>.md'
+
+    :param formatted_post_title: Post title formatted via format_post_title()
+    :param formatted_date_string: Date string formatted via validate_and_format_date_arg()
+
+    :return: Formatted filename for markdown file.
+    '''
     return f'{formatted_date_string}-{formatted_post_title}.md'
 
 
 def create_post_file(filename):
     '''Create the new post file with empty frontmatter followed by a newline.
 
-    Returns the path of the file that was written to.
+    Will create this file in the _posts/ directory.
+
+    :param filename: Formatted filename for post markdown file.
+
+    :return: The relative path of the file that was created.
     '''
     # TODO: we should probably make sure this is running from the root dir
     filepath = os.path.join('_posts', filename)
@@ -61,6 +81,8 @@ def open_file_in_editor(filepath):
     Pulls editor command from EDITOR env var, defaults to vim if unset.
 
     Will jump to the bottom of the file and enter insert mode on open.
+
+    :param filepath: Relative path to the post file to open.
     '''
     # Get the configured $EDITOR, default to vim if not set
     editor_command = os.environ.get('EDITOR', 'vim')
@@ -70,10 +92,12 @@ def open_file_in_editor(filepath):
     os.system(command)
 
 
+# ================================================================================
+# Script Command Class
+# ================================================================================
+
 class NewPostScript(ScriptBase):
     '''Create a new post and open it in editor.'''
-
-    # argparse stuff
 
     command = 'new-post'
 
@@ -102,7 +126,7 @@ class NewPostScript(ScriptBase):
         return parser
 
     def run(self):
-        # TODO: document
+        '''Parse and validate args, then create a new markdown file for a post and open it in editor.'''
         # Format post title text
         formatted_post_title = format_post_title(self.parsed_args.post_title)
         # Validate date
@@ -118,60 +142,16 @@ class NewPostScript(ScriptBase):
         print('Opening file in editor...')
         open_file_in_editor(filepath)
 
-
-# TODO: ================================================================================
-# TODO: REMOVE AFTER IMPLEMENTING CLASS:
-# TODO: ================================================================================
-
-def get_parser():
-    # TODO: usage description and all that
-    parser = argparse.ArgumentParser()
-
-    # Positional
-    parser.add_argument(
-        'post_title',
-        # TODO: usage text is funky with this:
-        metavar='<post title>',
-        help='Title to use when generating the filename. Can be multiple space-separated words',
-        nargs='+'
-    )
-
-    # Optional
-    parser.add_argument(
-        '-d', '--date',
-        metavar='<YYYY-MM-DD>',
-        help='Use a custom date for post. Defaults to current date',
-        default=datetime.now().strftime(DATE_STRING_FMT)
-    )
-
-    return parser
-
-
-# Main =========================================================================
+# ================================================================================
+# Main
+# ================================================================================
 
 def main():
-    parser = get_parser()
+    '''Parse args and run script.'''
+    parser = NewPostScript.get_parser()
     parsed_args = parser.parse_args()
     script = NewPostScript(parsed_args)
     script.run()
-
-    # TODO: ================================================================================
-    # TODO: REMOVE AFTER IMPLEMENTING CLASS:
-    # TODO: ================================================================================
-    # # Format post title text
-    # formatted_post_title = format_post_title(parsed_args.post_title)
-    # # Validate date
-    # formatted_date_string = validate_and_format_date_arg(parsed_args.date)
-    # if formatted_date_string is None:
-    #     print(f'Error: Unable to parse provided date "{parsed_args.date}", please use YYYY-MM-DD format.')
-    #     return
-    # # Format filename
-    # filename = format_filename(formatted_post_title, formatted_date_string)
-    # # Create the file
-    # filepath = create_post_file(filename)
-    # print(f'File created at "{filepath}"')
-    # print('Opening file in editor...')
-    # open_file_in_editor(filepath)
 
 
 if __name__ == '__main__':
